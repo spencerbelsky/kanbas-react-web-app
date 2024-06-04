805dd3522bff5368d0b688df37a513b4a142b254
Spencer Belsky <spencerbelsky@gmail.com>
2024-06-03 20:10:23 -0400

    final a3 changes

--- src/Kanbas/Courses/Grades/index.tsx
+++ src/Kanbas/Courses/Grades/index.tsx
@@ -7,26 +7,30 @@ import * as db from "../../Database";
 
 export default function Grades() {
     const currParams = useParams();
-    const cid = currParams.id;
+    const courseId = currParams.id;
     const grades = db.default.grades;
     const users = db.default.users;
-    const enrollments = db.default.enrollments;
+    //const enrollments = db.default.enrollments;
 
-    console.log("enrollments", enrollments)
-    const currStudents = enrollments.filter((enrollment: any) => enrollment.course === cid);
-    console.log("currStudents", currStudents)
-    console.log("grades", grades)
+    // console.log("enrollments", enrollments)
+    // console.log("users", users)
+    // const currStudents = enrollments.filter((enrollment: any) => enrollment.course === cid);
+    // console.log("currStudents", currStudents)
+    // console.log("grades", grades)
 
-    const currGrades = [];
-    for (let i = 0; i < currStudents.length; i++) {
-        console.log(currStudents[i].user)
-        const studentGrades = grades.filter((grade: any) => grade.student === currStudents[i].user);
-        if (studentGrades) {
-            currGrades.push(studentGrades);
-        }
-    }
+    // const currGrades = [];
+    // for (let i = 0; i < currStudents.length; i++) {
+    //     console.log(currStudents[i].user)
+    //     const studentGrades = grades.filter((grade: any) => grade.student === currStudents[i].user);
+    //     if (studentGrades) {
+    //         currGrades.push(studentGrades);
+    //     }
+    // }
 
-    console.log("currGrades", currGrades)
+    // console.log("currGrades", currGrades)
+
+    const assignments = db.default.assignments.filter((assignment) => assignment.course === courseId);
+    const enrollments = db.default.enrollments.filter((enrollment) => enrollment.course === courseId);
 
 
     return (
@@ -67,80 +71,33 @@ export default function Grades() {
                 <div className="col-auto">
                     <button id="wd-import-grades" className="btn btn-lg btn-secondary me-1 float-end">
                         <FaFilter className="position-relative me-2" style={{ bottom: "1px" }} />
-                        Apply Fiters
+                        Apply Filters
                     </button>
                 </div>
             </div>
 
 
             <div className="table-responsive">
-
                 <table className="table table-striped table-bordered">
+                    <thead>
+                        <th>Student Name</th>
+                        {assignments.map((assignment) => (<th>{assignment.title}</th>))}
+                    </thead>
                     <tbody>
-                        <tr>
-                            <th className="text-left">Student Name</th>
-                            <td className="text-center">A1 SETUP <br /> Out of 100</td>
-                            <td className="text-center">A2 HTML <br /> Out of 100</td>
-                            <td className="text-center">A3 CSS <br /> Out of 100</td>
-                            <td className="text-center">A4 BOOTSTRAP <br /> Out of 100</td>
-                        </tr>
-
-
-                        <tr>
-                            <td style={{ color: 'red' }}>Spencer Belsky</td>
-                            <td className="d-flex justify-content-center"><input type="number" defaultValue={69} className=" form-control form-control-sm text-center" style={{ width: '70px' }}></input></td>
-                            <td className="text-center">100%</td>
-                            <td className="text-center">100%</td>
-                            <td className="text-center">100%</td>
-                        </tr>
-                        <tr>
-                            <td style={{ color: 'red' }}>Paul Atreides</td>
-                            <td className="text-center">99%</td>
-                            <td className="text-center">99%</td>
-                            <td className="text-center">99%</td>
-                            <td className="text-center">99%</td>
-                        </tr>
-                        <tr>
-                            <td style={{ color: 'red' }}>Winston Smith</td>
-                            <td className="text-center">98%</td>
-                            <td className="text-center">98%</td>
-                            <td className="text-center">98%</td>
-                            <td className="text-center">98%</td>
-                        </tr>
-                        <tr>
-                            <td style={{ color: 'red' }}>Barbra Striesland</td>
-                            <td className="text-center">97%</td>
-                            <td className="text-center">97%</td>
-                            <td className="text-center">97%</td>
-                            <td className="text-center">97%</td>
-                        </tr>
-                        <tr>
-                            <td style={{ color: 'red' }}>Ofra Katz</td>
-                            <td className="text-center">90%</td>
-                            <td className="text-center">90%</td>
-                            <td className="text-center">90%</td>
-                            <td className="text-center">90%</td>
-                        </tr>
+                        {enrollments.map((enrollment) => {
+                            const currUser = db.default.users.find((user) => user._id === enrollment.user);
+                            return (
+                                <tr>
+                                    <td style={{ color: 'red' }}>{currUser?.firstName} {currUser?.lastName}</td> 
+                                    {assignments.map((assignment) => {
+                                        const currGrade = db.default.grades.find(
+                                            (grade) => grade.student === enrollment.user && grade.assignment === assignment._id);
+                                        return (<td  className="text-center">{currGrade?.grade || ""}%</td>);
+                                    })}
+                                </tr>);
+                        })}
                     </tbody>
                 </table>
             </div>
-
-            <div className="table table-striped table-bordered">
-
-                <div className="col">
-                    <div className="row py-3">
-                        <b>Student Name</b>
-                    </div>
-{/*                     
-                    {currGrades.map((student) => (
-                        <td className="text-left">{student} <br /> Out of 100</td>
-                    ))} */}
-                </div>
-                <div className="col">
-                    Assignment Names
-                </div>
-            </div>
-        </div>
-
-    )
+        </div>);
 }
